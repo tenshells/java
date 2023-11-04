60dae7486325d381ce0fbd9e8ae7e4f850284fd3
Shelton Dany <shelton.dany@gmail.com>
2023-11-04 23:58:35 +0530

    print utility

---
+++ printUtil.java
@@ -0,0 +1,9 @@
+public class printUtil {
+    public static void print(String s){
+        System.out.print(s);
+    }
+
+    public static void printl(String s){
+        System.out.println(s);
+    }
+}
